12a8e205abf1bfd4a651d8068197b9da969df87c
gitmhd <git.mhd@gmail.com>
2021-06-30 22:47:06 +0200

    First commit

---
+++ documentation/js/custom.js
@@ -0,0 +1,14 @@
+// Create instance of HUBSlider
+let doc = new HUBSlider();
+
+// Initialize the slider
+doc.init("doc");
+
+// Set options to slider
+doc.options({
+   slidingPrev: "hub-bottom",
+   slidingNext: "hub-top",
+   autoSlide: false,
+   animateSlide: false,
+   arrowsRemoveStyle: "both",
+});
